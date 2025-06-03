c41a0196f53099129e0a4f4d311fc618967de6dd
Saheli Mahapatra <89475482+sahelii@users.noreply.github.com>
2025-06-03 19:06:10 +0530

    added render domain to allowed hosts  for deployement

--- CashFlowPulse/settings.py
+++ CashFlowPulse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*nf6gs&0wimqz!gx@u*op7+#m$t53j(jd&-672rv)d6zi_4jrb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
